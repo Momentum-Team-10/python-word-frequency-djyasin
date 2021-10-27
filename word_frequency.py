047fd2a4d137b0571ca48e12caa22a3fb9c978fd
Lila <djyasin.ly@gmail.com>
2021-10-27 13:57:44 -0400

    Got my string to make lines at the word levelgit add .git add .

--- word_frequency.py
+++ word_frequency.py
@@ -1,16 +1,17 @@
+import string
 STOP_WORDS = [
     'a', 'an', 'and', 'are', 'as', 'at', 'be', 'by', 'for', 'from', 'has', 'he',
     'i', 'in', 'is', 'it', 'its', 'of', 'on', 'that', 'the', 'to', 'were',
     'will', 'with'
 ]
-import string
+
 
 def print_word_freq(file):
     """Read in `file` and print out the frequency of words in that file."""
     with open(file) as text:
-        lines = text.readlines()
-        print(f"{len(lines)} lines in the file.")
-        for line in lines: 
+        line = text.read()
+        print(f"{len(line)} lines in the file.")
+        for text in line: 
             line = line.replace(",", "")
             line = line.replace(".", "")
             line = line.replace("-", "")
@@ -19,15 +20,19 @@ def print_word_freq(file):
             line = line.replace("'", "")
             line = line.replace("'", "")
             line = line.lower()
+            line = line.split(" ")
             print(line)
-"""can't figure out how to remove the STOP_WORDS so want to write a for loop to omit them from the word count"""
-
-def word_count(word):
-    """pass over STOP_WORDS"""
-    if word in STOP_WORDS:
-        pass
-    else:
-"""somehow count words"""
+#"""can't figure out how to remove the STOP_WORDS so want to write an if else statement to omit them from the word count"""
+#"""pass over STOP_WORDS"""
+#def word_pass(file):
+#   with open(file) as text:
+#        lines = text.readlines()
+#        print(f"{len(lines)} lines in the file.")
+#           if word in STOP_WORDS:
+#                 pass
+#   else:
+#"""somehow count words"""
+#Are we trying to get thee exact words from the stop list, OR get the top frequencies
 
 
 
