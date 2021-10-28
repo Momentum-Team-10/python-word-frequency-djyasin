154d7e34b4f8169e06976c8916b2a939c1bb1887
Lila <djyasin.ly@gmail.com>
2021-10-28 09:38:42 -0400

    One of my group members was able to get the word count function working last night. We are going to get  together today to review that.

--- word_frequency.py
+++ word_frequency.py
@@ -11,29 +11,23 @@ def print_word_freq(file):
     with open(file) as text:
         line = text.read()
         print(f"{len(line)} lines in the file.")
-        for text in line: 
+#"""calling text rather than line to enable it to form word list instead of line list"""
+        for text in line:
             line = line.replace(",", "")
             line = line.replace(".", "")
             line = line.replace("-", "")
             line = line.replace("?", "")
             line = line.replace(":", "")
             line = line.replace("'", "")
-            line = line.replace("'", "")
+            line = line.replace(";", "")
+            line = line.replace("/n", "")
             line = line.lower()
             line = line.split(" ")
+            line = file.count("love")
             print(line)
-#"""can't figure out how to remove the STOP_WORDS so want to write an if else statement to omit them from the word count"""
-#"""pass over STOP_WORDS"""
-#def word_pass(file):
-#   with open(file) as text:
-#        lines = text.readlines()
-#        print(f"{len(lines)} lines in the file.")
-#           if word in STOP_WORDS:
-#                 pass
-#   else:
 #"""somehow count words"""
-#Are we trying to get thee exact words from the stop list, OR get the top frequencies
-
+# (file).remove STOP_Words
+# Are we trying to get thee exact words from the stop list, OR get the top frequencies
 
 
 if __name__ == "__main__":
@@ -48,7 +42,7 @@ if __name__ == "__main__":
 """"this calls the file to run"""
 file = Path(args.file)
 if file.is_file():
-        print_word_freq(file)
+    print_word_freq(file)
 else:
-        print(f"{file} does not exist!")
-        exit(1)
+    print(f"{file} does not exist!")
+    exit(1)
